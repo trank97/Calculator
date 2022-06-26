baafd72bddc1aa6d81c80661e538e66c325451e0
trân vòng <vongkientran@gmail.com>
2022-06-25 22:13:25 -0700

    post my project

---
+++ calculator/src/calculator/Calculator.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './Calculator.css'
+
+export default class Calculator extends Component {
+
+  state = {
+    resultCalcu: '0',
+    typingElement: '',
+  }
+  typingNumber = (value) => {
+
+    let operator = ['+', '-', '*', '/', '.'];
+    // if(operator.includes(value)){
+    //   console.log('chuc mung')
+    // }
+    // let indexLast = operator.indexOf(value);
+    // console.log(indexLast)
+    if (this.state.typingElement[this.state.typingElement.length - 1] === '+' && this.state.typingElement[this.state.typingElement.length - 1] === value) {
+      value = ''
+    }
+    if (this.state.typingElement[this.state.typingElement.length - 1] === '-' && this.state.typingElement[this.state.typingElement.length - 1] === value) {
+      value = ''
+    } if (this.state.typingElement[this.state.typingElement.length - 1] === '*' && this.state.typingElement[this.state.typingElement.length - 1] === value) {
+      value = ''
+    } if (this.state.typingElement[this.state.typingElement.length - 1] === '/' && this.state.typingElement[this.state.typingElement.length - 1] === value) {
+      value = ''
+    } if (this.state.typingElement[this.state.typingElement.length - 1] === '.' && this.state.typingElement[this.state.typingElement.length - 1] === value) {
+      value = ''
+    }
+
+
+    setTimeout(() => {
+      this.setState({
+        typingElement: this.state.typingElement + (value),
+      });
+    })
+
+
+
+
+  }
+
+  allClear = () => {
+    this.setState({
+      resultCalcu: '0',
+      typingElement: '',
+    })
+  }
+
+  delete = () => {
+    this.setState({
+      typingElement: this.state.typingElement.slice(0, -1)
+    });
+  }
+
+  caculate = () => {
+    try {
+      this.setState({
+        typingElement: eval(this.state.typingElement.replace(/^0+/, '')).toString(),
+        resultCalcu: this.state.typingElement + '=',
+      });
+      setTimeout(() => {
+        console.log(typeof this.state.typingElement)
+      })
+    } catch (err) {
+      this.setState({
+        resultCalcu: 'Please press the all clear(AC) button!',
+        typingElement: 'ERROR'
+      })
+    }
+  }
+
+  render() {
+
+    return (
+      <div className='container grid-container'>
+        <div className='grid-item output'>
+          <div data-result className='result'>{this.state.resultCalcu}</div>
+          <div data-typing className='typing'>{this.state.typingElement} </div>
+        </div>
+        <button onClick={() => { this.typingNumber(7) }} className='grid-item'>7</button>
+        <button onClick={() => { this.typingNumber(8) }} className='grid-item'>8</button>
+        <button onClick={() => { this.typingNumber(9) }} className='grid-item'>9</button>
+        <button onClick={() => { this.typingNumber('/') }} className='grid-item action'>/</button>
+        <button className='grid-item action' onClick={() => { this.delete() }}>DEL</button>
+        <button onClick={() => { this.typingNumber(4) }} className='grid-item'>4</button>
+        <button onClick={() => { this.typingNumber(5) }} className='grid-item'>5</button>
+        <button onClick={() => { this.typingNumber(6) }} className='grid-item'>6</button>
+        <button onClick={() => { this.typingNumber('*') }} className='grid-item action'>*</button>
+        <button className='grid-item action' onClick={() => { this.allClear() }} >AC</button>
+        <button onClick={() => { this.typingNumber(1) }} className='grid-item'>1</button>
+        <button onClick={() => { this.typingNumber(2) }} className='grid-item'>2</button>
+        <button onClick={() => { this.typingNumber(3) }} className='grid-item'>3</button>
+        <button onClick={() => { this.typingNumber('-') }} className='grid-item action'>-</button>
+        <button className='grid-item item-2 action' onClick={() => { this.caculate() }}>=</button>
+        <button onClick={() => { this.typingNumber(0) }} className='grid-item item-1'>0</button>
+        <button onClick={() => { this.typingNumber('.') }} className='grid-item'>.</button>
+        <button onClick={() => { this.typingNumber('+') }} className='grid-item action'>+</button>
+      </div>
+    )
+  }
+}
